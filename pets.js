51fbb4b25edd3120c418f44eb7095d1502efa158
Joshua McMiller <jjmcmi113r@gmail.com>
2022-05-04 23:58:00 -0500

    added pseudo code to the pets.js file

--- pets.js
+++ pets.js
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-
+//assign process.argv to a variable
 const command = process.argv
-
+//assign index to a variable
 let indexNum = process.argv[3]
+//assign filesystem module to a variable
 const fs = require('fs')
-// const json = require('./pets.json')
-// let jsonResult
 
-// if (!command[2]){
-//     console.error('Usage: node pets.js [read | create | update | destroy]')
-//     process.exit(1);
-    
-// }
 
-// console.log(json)
 
 start(command)
+
+//create a start function that will pull data initially and pass that data on to other functions based on the command given
 function start(command){
     fs.readFile('pets.json', 'utf-8', (err, data) => {
         let jsonResult = JSON.parse(data)
@@ -40,7 +35,7 @@ function start(command){
         }
     })
 }
-
+//read command error function
 function read(command, jsonResult){
     if (command[2] === 'read'){
         
@@ -58,28 +53,28 @@ function read(command, jsonResult){
             else {
             console.log(jsonResult[indexNum])
             }
-       // console.log(jsonResult[indexNum])
-        // });
+       
     }
 
 }
-
+// this function creates a pet object, then combines the new object with the current array. Then writes the new array to the json file
 function create(command, jsonResult){
     
-    // read(command)
     if(command[5] === undefined || command[4] === undefined && command[5] === undefined || command[3] === undefined && command[4] === undefined && command[5] === undefined) {
             console.error('Usage: node pets.js create AGE KIND NAME')
             process.exit(1)
         }
         else{
-        let petArray = []    
+        let petArray = []  
+        
+        //create pet object and assign values
         let createPet = {}
         createPet.age = parseInt(command[3])
         createPet.kind = command[4]
         createPet.name = command[5]
-        // let jsonResult = JSON.parse(data)
+        // push the new pet to the current json array
         jsonResult.push(createPet)
-        // petArray.push(jsonResult)
+        // writes new json array to the pets.json file
         fs.writeFile('pets.json', JSON.stringify(jsonResult), function (err) {
             if (err) {
                 console.log(err)
@@ -100,11 +95,12 @@ function update(command, jsonResult){
         process.exit(1)
     }
     else {
+        // access current array element based on user input, update the object 
         let current = jsonResult[indexNum]
         current.age = parseInt(command[4])
         current.kind = command[5]
         current.name = command[6]
-
+        // write updated object to the json file
         fs.writeFile('pets.json', JSON.stringify(jsonResult), function (err) {
             if (err) {
                 console.log(err)
@@ -123,6 +119,7 @@ function destroy(command, jsonResult){
         process.exit(1)
     }
     else {
+        // remove element at index and write the file 
         jsonResult.splice(indexNum,1)
         fs.writeFile('pets.json', JSON.stringify(jsonResult), function (err) {
             if (err) {
